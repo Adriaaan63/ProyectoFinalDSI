3baaf14e9c74f7a292644385cdcf034f5a7f4737
Adriaan63 <Adcast07@ucm.es>
2024-05-08 12:18:13 +0200

    se puede elegir al personaje

--- Assets/MenuCharacter.cs
+++ Assets/MenuCharacter.cs
@@ -9,6 +9,11 @@ public class MenuCharacter : MonoBehaviour
     Stats attack;
     TextField defenseStats;
     Stats1 defense;
+    TextField input_name;
+    Label nameLabel;
+
+    VisualElement imagePrin;
+    VisualElement skin1, skin2, skin3, skin4;
     private void OnEnable()
     {
         VisualElement root = GetComponent<UIDocument>().rootVisualElement;
@@ -16,9 +21,22 @@ public class MenuCharacter : MonoBehaviour
         attack = root.Q<Stats>("Stats");
         defenseStats = root.Q<TextField>("DefenseText");
         defense = root.Q<Stats1>("Stats1");
+        input_name = root.Q<TextField>("NameText");
+        nameLabel = root.Q<Label>("Nombre");
+
+        imagePrin = root.Q<VisualElement>("skin");
+        skin1 = root.Q<VisualElement>("P1");
+        skin2 = root.Q<VisualElement>("P2");
+        skin3 = root.Q<VisualElement>("P3");
+        skin4 = root.Q<VisualElement>("P4");
+
         attackStats.RegisterCallback<ChangeEvent<string>>(ChangeAttack);
         defenseStats.RegisterCallback<ChangeEvent<string>>(ChangeDefense);
-
+        input_name.RegisterCallback<ChangeEvent<string>>(ChangeName);
+        skin1.RegisterCallback<ClickEvent>(ChangeImage);
+        skin2.RegisterCallback<ClickEvent>(ChangeImage);
+        skin3.RegisterCallback<ClickEvent>(ChangeImage);
+        skin4.RegisterCallback<ClickEvent>(ChangeImage);
     }
     void ChangeAttack(ChangeEvent<string> e)
     {
@@ -43,5 +61,32 @@ public class MenuCharacter : MonoBehaviour
             defense.NivelEscudo = newDefenseValue;
         }
 
+    }
+    void ChangeName(ChangeEvent<string> e)
+    {
+        if(e.newValue.Length < 10)
+            nameLabel.text = e.newValue;
+    }
+    void ChangeImage(ClickEvent e)
+    {
+            VisualElement skins = e.target as VisualElement;
+        Sprite per = null;
+        if (skins == skin1)
+        {
+            per = Resources.Load<Sprite>("Images/MenuCreate/character1");
+        }
+        else if(skins == skin2)
+        {
+            per = Resources.Load<Sprite>("Images/MenuCreate/character2");
+        }
+        else if(skins == skin3)
+        {
+            per = Resources.Load<Sprite>("Images/MenuCreate/character3");
+        }
+        else if (skins == skin4)
+        {
+            per = Resources.Load<Sprite>("Images/MenuCreate/character4");
+        }
+            imagePrin.style.backgroundImage = new StyleBackground(per);
     }
 }
